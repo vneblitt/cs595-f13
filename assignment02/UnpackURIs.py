317caa65de8fc196b1e50ff1660c1fda596d2b30
Valentina <vneblitt@jessa.littleprojects.org>
2013-09-22 02:28:23 -0400

    Aaaaaaargh!

--- assignment02/UnpackURIs.py
+++ assignment02/UnpackURIs.py
@@ -1,6 +1,11 @@
 import urllib.request
 import urllib.parse
 import urllib.error
+import http.client
+import time
+
+start = time.localtime()
+print('Start time is: ' + time.asctime(start))
 
 #a = urllib.request.urlopen('http://goo.gl/LhYg3')
 
@@ -13,9 +18,23 @@ s = open('unpackedURLs.txt', 'w')
 #except urllib.error.HTTPError as e:
 #    print(e.code)
 
+'''for line in f:
+    f.readline()
+    try:
+        a = urllib.request.urlopen(line)
+        good = a.geturl()
+        s.write(good)
+        s.write('\n')
+    except urllib.error.HTTPError:
+        pass'''
+
+
 for line in f:
     f.readline()
     s.write(line)
 
 f.close()
 s.close()
+
+finish = time.localtime()
+print('Done. Finish time is: ' + time.asctime(finish))
