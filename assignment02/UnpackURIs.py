fa941d419d1808ee03c64cd1eab388e5c8bbf041
Valentina <vneblitt@jessa.littleprojects.org>
2013-09-22 00:06:03 -0400

    Midway on unshortening

--- assignment02/UnpackURIs.py
+++ assignment02/UnpackURIs.py
@@ -1,2 +1,21 @@
 import urllib.request
 import urllib.parse
+import urllib.error
+
+#a = urllib.request.urlopen('http://goo.gl/LhYg3')
+
+f = open('tweetlink.txt', 'r')
+s = open('unpackedURLs.txt', 'w')
+
+#try:
+#    print(a.geturl())
+
+#except urllib.error.HTTPError as e:
+#    print(e.code)
+
+for line in f:
+    f.readline()
+    s.write(line)
+
+f.close()
+s.close()
