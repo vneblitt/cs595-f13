f26edc7fb74b7f0fe833ac00a1fb73bf323d40dd
Valentina <vneblitt@jessa.littleprojects.org>
2013-10-09 21:14:34 -0400

    Proof of concept on single file

--- assignment04/q2/CreateDotFile.py
+++ assignment04/q2/CreateDotFile.py
@@ -3,3 +3,23 @@
 # Fall 2013
 # Assignment #4 Question #2
 
+f = open('0bb6e775e10d833313f9bce6ef8711b0', 'r')
+s = open('/Users/vneblitt/Documents/cs595-f13/assignment04/q2/mydotfile.dot', 'w')
+
+mylines = f.readlines()
+
+site = mylines[1].strip('\n')
+links = mylines[3:]
+
+s.write('digraph spaghetti {\n')
+
+for link in mylines:
+    link = link.strip()
+    s.write('"' + site + '"' + ' -> ' + '"' + link + '"' + ' [ label = "' + site + ' to ' + link + '" ]; \n')
+
+s.write('}')
+
+f.close()
+s.close()
+
+    
