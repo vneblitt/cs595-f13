687f46640963773a617f4b254d6edf9c02ad262e
Valentina <vneblitt@jessa.littleprojects.org>
2013-10-13 21:57:56 -0400

    Added Extra Credit 2

---
+++ assignment05/e2/FollowingCountGraph.R
@@ -0,0 +1,20 @@
+# Valentina Neblitt-Jones
+# CS 595 Introduction to Web Science
+# Fall 2013
+# Assignment 5 Extra Credit 2
+
+followingdata <- read.csv("/Users/vneblitt/Documents/cs595-f13/assignment05/e2/followingreport.csv", header = TRUE)
+
+fCount <- sort(followingdata$FollowingCount)
+
+max_y <- max(fCount) + 500
+
+cols <- c("slategray3", "red")
+
+ndx = order(followingdata$FollowingCount)
+
+fd_sorted = followingdata[ndx,]$ScreenName
+
+pos <- fd_sorted == "phonedude_mln"
+
+barplot(fCount, main="Followers' Following Counts", xlab = "Followers", ylab = "Following Count", border = NA, ylim = c(0, max_y), col = cols[pos + 1])
