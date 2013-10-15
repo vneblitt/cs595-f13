6a22319af2689fbb2cff415d6b6ed44c7764dd28
Valentina <vneblitt@hannah.littleprojects.org>
2013-10-14 21:41:34 -0400

    Added mean, median, sd to answers

--- assignment05/e2/FollowingCountGraph.R
+++ assignment05/e2/FollowingCountGraph.R
@@ -18,3 +18,13 @@ fd_sorted = followingdata[ndx,]$ScreenName
 pos <- fd_sorted == "phonedude_mln"
 
 barplot(fCount, main="Followers' Following Counts", xlab = "Followers", ylab = "Following Count", border = NA, ylim = c(0, max_y), col = cols[pos + 1])
+
+fmean <- mean(fCount)
+
+fsd <- sd(fCount)
+
+fmedian <- median(fCount)
+
+write(paste("The mean is", fmean), stdout())
+write(paste("The median is", fmedian), stdout())
+write(paste("The standard deviation is", fsd), stdout())
--- assignment05/q1/FriendCountGraph.R
+++ assignment05/q1/FriendCountGraph.R
@@ -18,3 +18,13 @@ fd_sorted = friendsdata[ndx,]$Identifier
 pos <- fd_sorted == "Valentina"
 
 barplot(friendCount, main="Friends' Friend Counts", xlab = "Friends", ylab = "Friend Count", border = NA, ylim = c(0, max_y), col = cols[pos + 1])
+
+fmean <- mean(friendCount)
+
+fsd <- sd(friendCount)
+
+fmedian <- median(friendCount)
+
+write(paste("The mean is", fmean), stdout())
+write(paste("The median is", fmedian), stdout())
+write(paste("The standard deviation is", fsd), stdout())
