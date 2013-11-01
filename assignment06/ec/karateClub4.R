c22e6a978a05b5278abc73572c11d96f945f30f7
Valentina <vneblitt@jessa.littleprojects.org>
2013-10-31 23:24:18 -0400

    Final Edition

---
+++ assignment06/ec/karateClub4.R
@@ -0,0 +1,20 @@
+# Valentina Neblitt-Jones
+# CS 595 Introduction to Web Science
+# Fall 2013
+# Assignment 6 Extra Credit
+
+data(karate)
+k <- karate
+plot.igraph(k)
+
+repeat{
+kedge <- edge.betweenness(k)
+korder <- order(kedge, decreasing=TRUE)
+a <- korder[-1]
+b <- get.edge(k,a)
+k <- delete.edges(k, E(k,P=b))
+plot.igraph(k)
+
+# 4 Clusters
+if (clusters(k)$no == 4) break()
+}
