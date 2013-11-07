55728f0572f2927be6a15b338e40a1610136ebb1
Valentina <vneblitt@hannah.littleprojects.org>
2013-11-07 00:26:20 -0500

    Created JSON file for split

--- assignment07/q1/CreateJSONFile02.py
+++ assignment07/q1/CreateJSONFile02.py
@@ -6,11 +6,13 @@
 
 import json
 import pprint
+import networkx
 from networkx.readwrite import json_graph
 
 pp = pprint.PrettyPrinter()
 
 f = open('zachary1.json')
+g = open('zachary2.json', 'w')
 
 data = json.load(f)
 # pp.pprint(data)
@@ -24,6 +26,21 @@ G = json_graph.node_link_graph(data)
 
 # repeat{
 # kedge <- edge.betweenness(k)
+while networkx.number_connected_components(G) < 2:
+	edgy = networkx.edge_betweenness_centrality(G)
+# pp.pprint(edgy)
+	maxb = 0
+	for edge in edgy:
+		#print('key ' + str(edge) + ' value ' + str(edgy[edge]))
+		if edgy[edge] > maxb:
+			maxb = edgy[edge]
+			maxedge = edge
+# print(maxb)
+# print(maxedge)
+	G.remove_edge(maxedge[0],maxedge[1])
+
+# pp.pprint(G.edges())
+
 # korder <- order(kedge, decreasing=TRUE)
 # a <- korder[-1]
 # b <- get.edge(k,a)
@@ -32,5 +49,11 @@ G = json_graph.node_link_graph(data)
 # if (clusters(k)$no == 2) break()
 # }
 
+result = json_graph.node_link_data(G)
+
+# print(json.dumps(result, indent=4))
+
+g.write(json.dumps(result, indent=4))
+g.close()
 
     
