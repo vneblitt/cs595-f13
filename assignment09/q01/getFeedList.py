3673763eaf9ea91d450e7b4a7ef19c77a8036cbd
Valentina <vneblitt@jessa.littleprojects.org>
2013-12-01 19:58:24 -0500

    Revised getFeedList.py and added generatefeedvector.py

--- assignment09/q01/getFeedList.py
+++ assignment09/q01/getFeedList.py
@@ -4,34 +4,47 @@
 # Assignment #10 Question #1
 
 from bs4 import BeautifulSoup
+from urllib2 import HTTPError
 import urllib2
-import feedparser
-import re
 import time
+
+f = open('feedlist.txt', 'w')
 
 # These two are automatically in the list
 firstfeed = 'http://f-measure.blogspot.com/feeds/posts/default'
 secondfeed = 'http://ws-dl.blogspot.com/feeds/posts/default'
 
-print firstfeed
-print secondfeed
+feedlist = []
+feedlist.append(firstfeed)
+feedlist.append(secondfeed)
 
-count = 0
+while len(feedlist) <= 100:
+    try:
 
-while count < 101:
+        nextblog = 'http://www.blogger.com/next-blog?navBar=true&blogID=3471633091411211117'
 
-    nextblog = 'http://www.blogger.com/next-blog?navBar=true&blogID=3471633091411211117'
+        html = urllib2.urlopen(nextblog).read()
 
-    html = urllib2.urlopen(nextblog).read()
+        soup = BeautifulSoup(html)
 
-    soup = BeautifulSoup(html)
+        atomfeedurl = soup.find_all('link', attrs = {'type' : 'application/atom+xml'})[0].attrs['href']
 
-    atomfeedurl = soup.find_all('link', attrs = {'type' : 'application/atom+xml'})[0].attrs['href']
+    except HTTPError as h:
+        pass
 
-    print atomfeedurl
+    except IndexError as i:
+        pass
 
-    count = count + 1
+    if atomfeedurl:
+
+        if atomfeedurl not in feedlist:
+
+            print atomfeedurl
+            feedlist.append(atomfeedurl)
 
     time.sleep(1)
 
+for item in feedlist:
+    f.write(item + '\n')
 
+f.close()
